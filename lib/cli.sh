ebbf093ee99566f0e804c0c8418dbcf79ed38c27
Johnny C. Lam <jlam88@gmail.com>
2023-02-08 23:41:37 -0500

    fix: don't overwrite user config changes with default theme
    The default theme was loaded after the user configuration file was read,
    which led to user changes being overwritten by default values.  Reverse
    the order of operations so that the default theme is loaded first and
    the user configuration file is read afterwards to modify the existing
    theme or to replace it outright.
    
    Drop support for `${STEAMSHIP_THEME}` variable. To set the theme from
    the configuration file, simply run `steamship theme <name>` to load the
    named theme.

--- lib/cli.sh
+++ lib/cli.sh
@@ -8,24 +8,36 @@ steamship_load_library prompt
 steamship_load_library themes
 
 steamship() {
+	sscli_do_refresh=
 	case ${1} in
 	refresh)
 		# Rebuild prompt strings after configuration changes.
-		steamship_prompt_refresh
+		sscli_do_refresh='yes'
 		;;
 	reset)
 		# Reset to default settings.
 		steamship_modules_reset
+		# Load the default theme settings first.
+		steamship_load_theme starship
+		# Load the user configuration file to modify the starship theme.
+		# The user configuration file may possibly invoke "steamship theme"
+		# to load another theme altogether.
 		steamship_config
-		steamship_load_theme "${STEAMSHIP_THEME:-"starship"}"
-		steamship_prompt_refresh
+		sscli_do_refresh='yes'
 		;;
 	theme)
 		shift
 		steamship_load_theme "${@}"
-		steamship_prompt_refresh
+		sscli_do_refresh='yes'
 		;;
 	esac
+	# Refresh as final command if requested, but only if not called from
+	# within the configuration file.
+	if	[ -n "${sscli_do_refresh}" ] &&
+		[ -z "${steamship_config_sourcing}" ]
+	then
+		steamship_prompt_refresh
+	fi
 }
 
 STEAMSHIP_LIBS_SOURCED="${STEAMSHIP_LIBS_SOURCED} cli"
